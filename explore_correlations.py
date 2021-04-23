b5f779a507c373fdb439a64c7f17c51c4426e03b
DigitalJay <pushkareva_lisa@mail.ru>
2021-04-23 21:45:36 +0500

    Generation is so nice now

---
+++ explore_correlations.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from fridge_util import Fridge
+
+ingredients_i_have = ["garlic", "milk", "chicken", "pepper", "cabbage", "cod", "cheese"]
+
+correlation_matrix=pd.read_csv("correlations.csv", delimiter=',').to_numpy()
+fridge=Fridge()
+
+ingredients_indexes = fridge.ings2indexes(ingredients_i_have)
+for inga in ingredients_indexes:
+    for ingb in ingredients_indexes:
+        print(fridge.indexes2ings([inga, ingb]), correlation_matrix[inga][ingb+1])
--- generation.py
+++ generation.py
@@ -21,9 +21,13 @@ class Generator():
         return topn
 
     def count_correlation(self, indexes):
+        if len(indexes)==1:
+            return 100
         correlations=[]
         for inda in range(len(indexes)):
-            for indb in range(inda, len(indexes)):
-                correlations.append(self.correlation_matrix[inda][indb+1])
+            for indb in range(inda+1, len(indexes)):
+                if self.correlation_matrix[indexes[inda]][indexes[indb]+1]<100:
+                    return 0
+                correlations.append(self.correlation_matrix[indexes[inda]][indexes[indb]+1])
         return sum(correlations)/len(correlations)
 
